0c8b83ad70519816a5b2542a16adfa6a9e2b3e9c
0x78654C <l2xguard@live.com>
2020-07-10 15:30:52 +0300

    Lincense update

--- WakeOverLan_Android/app/src/main/java/com/example/administrator/wol/MainActivity.java
+++ WakeOverLan_Android/app/src/main/java/com/example/administrator/wol/MainActivity.java
@@ -38,11 +38,19 @@ public class MainActivity extends AppCompatActivity {
     
     Description: This a simple Wake Over Lan app that that came from the idea of code transparency
     and not closed source where you some times don't know what the app really dose even if is free or not..
-    This app is distributed under the GNU GPLv3 License .
     Usage: Is made for 1 machine only. You add the IP/Hostname(internal or external ), MAC address, and WOL port(Depending on your motherboard, the
     default port is 9 or can be changed by desire).
 
+    This app is distributed under the MIT License.
     Copyright © 2020 0x78654C. All rights reserved.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
     */
     SharedPreferences myPrefs;
