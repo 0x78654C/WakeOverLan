de78491ce909b3341d2c27f057e0a6a3511ba43c
x_coding <l2xguard@live.com>
2021-10-18 22:46:08 +0300

    Update AppDelegate.swift

--- WakeOverLAN_iOS/Wake_On_LAN_iOS/AppDelegate.swift
+++ WakeOverLAN_iOS/Wake_On_LAN_iOS/AppDelegate.swift
@@ -2,26 +2,25 @@
 //  AppDelegate.swift
 //  Wake_On_LAN_iOS
 //
- /* 
-    Author: 0x78654C
+ /* Copyright (c) 2017 Jesper Lindberg
 
-    Description: This a simple Wake Over Lan app that that came from the idea of code transparency
-    and not closed source where you some times don't know what the app really dose even if is free or not..
-    Usage: Is made for 1 machine only. You add the IP/Hostname(internal or external ), MAC address, and WOL port(Depending on your motherboard, the
-    default port is 9 or can be changed by desire).
-    Requirements: iOS 11+
-	Awake library from: https://github.com/jesper-lindberg/Awake   (thank you very much)
-	
-    This app is distributed under the MIT License.
-    Copyright © 2020 0x78654C. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
     */
 
 import UIKit
